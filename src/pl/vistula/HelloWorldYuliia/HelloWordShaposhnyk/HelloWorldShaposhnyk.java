7af49820bb1b081630efd7a5ea8e6c3917cd969b
yuliia <shaposhnyk.yu@gmail.com>
2021-10-07 16:27:35 +0200

    first commit

---
+++ src/pl/vistula/HelloWorldYuliia/HelloWordShaposhnyk/HelloWorldShaposhnyk.java
@@ -0,0 +1,9 @@
+package pl.vistula.HelloWorldYuliia.HelloWordShaposhnyk;
+
+public class HelloWorldShaposhnyk{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
