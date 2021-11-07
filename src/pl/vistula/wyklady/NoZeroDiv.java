448c2217933761d7f1f07d987699000984e1cedf
yuliia <shaposhnyk.yu@gmail.com>
2021-11-07 20:55:24 +0100

    Ternarny przyklad

---
+++ src/pl/vistula/wyklady/NoZeroDiv.java
@@ -0,0 +1,12 @@
+package pl.vistula.wyklady;
+
+public class NoZeroDiv {
+    public static void main(String[] args) {
+        int result;
+
+        for (int i = -5; i<6; i++){
+            result = i!=0 ? 100 / i: 0;
+            System.out.println("100 / " + i + "rowna się " + result);
+        }
+    }
+}
